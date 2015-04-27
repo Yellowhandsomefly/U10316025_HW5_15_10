bd700ee1e42e2f5705af585f6311531a38079a38
Yellowhandsomefly <I279KenEmily22@gmail.com>
2015-04-27 21:19:25 +0800

    update MyStack
    增加註解

--- MyStack.java
+++ MyStack.java
@@ -1,26 +1,35 @@
+
+
 import java.util.ArrayList;
 
 public class MyStack implements Cloneable{
+	
+	/**Declare a ArrayList*/
 	private ArrayList<Object> list = new ArrayList<Object>();
 
+	/**Return true if ArrayList is empty*/
 	public boolean isEmpty() {
 		return list.isEmpty();
 	}
 
+	/**Return the size of ArrayList*/
 	public int getSize() {
 		return list.size();
 	}
 
+	/**Return the last element*/
 	public Object peek() {
 		return list.get(getSize() - 1);
 	}
 
+	/**Return the last element and delete the last element*/
 	public Object pop() {
 		Object o = list.get(getSize() - 1);
 		list.remove(getSize() - 1);
 		return o;
 	}
 
+	/**Push the element*/
 	public void push(Object o) {
 		list.add(o);
 	}
@@ -31,8 +40,13 @@ public class MyStack implements Cloneable{
 	}
 
 	public Object clone(){
+		
 		try{
+			
+			/**Perform a shallow copy*/
 			MyStack stack = (MyStack)super.clone();
+			
+			/**Deep copy*/
 			stack.list = (ArrayList<Object>)(list.clone());
 			return stack;
 		}
